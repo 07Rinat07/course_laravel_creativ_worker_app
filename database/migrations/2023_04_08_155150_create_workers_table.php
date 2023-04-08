0d804ad6e0693a2abe499b3dffaf0c49b1bc0a9a
Rinat Sarmuldin <ura07srr@gmail.com>
2023-04-08 21:08:29 +0500

    Model and migration. Lesson_4.

---
+++ database/migrations/2023_04_08_155150_create_workers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workers', function (Blueprint $table) {
+            $table->id();
+            $table->string ('name');
+            $table->string ('surname');
+            $table->string ('email');
+            $table->integer ('age')->nullable ();
+            $table->text ('description')->nullable ();
+            $table->boolean ('is_married')->default (false);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workers');
+    }
+};
