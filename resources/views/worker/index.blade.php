f45b9871d0db99c293e07e2b8dc7853d92c429c3
Rinat Sarmuldin <ura07srr@gmail.com>
2023-04-09 23:19:05 +0500

    Pagination with a filter. Lesson_14.

--- resources/views/worker/index.blade.php
+++ resources/views/worker/index.blade.php
@@ -60,7 +60,7 @@ Index page
         <hr>
         @endforeach
             <div class="my-nav">
-                {{ $workers->links() }}
+                {{ $workers->withQueryString()->links() }}
             </div>
 </div>
 
